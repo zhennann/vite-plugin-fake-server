b1a89d3d5c1a06501090e4a7477a34b31289b842
zhennann <zhen.nann@icloud.com>
2024-06-22 17:35:56 +0800

    feat: patchConfig

--- packages/vite-plugin-fake-server/src/client/defineFakeRoute.ts
+++ packages/vite-plugin-fake-server/src/client/defineFakeRoute.ts
@@ -2,6 +2,18 @@ import type { FakeRoute } from "../node/types";
 
 export type FakeRouteConfig = FakeRoute[] | FakeRoute;
 
+function patchConfig(config: FakeRoute) {
+	if (!config.headers) config.headers = [];
+	if (!config.headers["Content-Type"]) {
+		config.headers["Content-Type"] = "application/json";
+	}
+}
+
 export function defineFakeRoute(config: FakeRouteConfig) {
+	if (Array.isArray(config)) {
+		config.forEach((item) => patchConfig(item));
+	} else {
+		patchConfig(config);
+	}
 	return config;
 }
