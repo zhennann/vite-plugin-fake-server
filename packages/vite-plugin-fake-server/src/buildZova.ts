a05047449c05ede1ce8813407acec08d3e293f3a
zhennann <zhen.nann@icloud.com>
2025-04-16 15:56:31 +0800

    feat: zova

---
+++ packages/vite-plugin-fake-server/src/buildZova.ts
@@ -0,0 +1,98 @@
+import { mkdir, writeFile, rm } from "node:fs/promises";
+import path, { join } from "node:path";
+import { existsSync } from "node:fs";
+
+import type { ResolvedConfig } from "vite";
+import * as esbuild from "esbuild";
+import pc from "picocolors";
+
+import { name } from "../package.json";
+
+import type { ServerBuildOptions } from "./types.js";
+import type { ResolvePluginOptionsType } from "./resolvePluginOptions.js";
+
+export const PORT = 8888;
+export const OUTPUT_DIR = "fakeServer";
+
+export async function generateFakeServer(options: ResolvePluginOptionsType, config: ResolvedConfig) {
+	const buildOptions = options.build === true ? { port: PORT, outDir: OUTPUT_DIR } : options.build;
+
+	const { port = PORT, outDir = OUTPUT_DIR } = buildOptions as Required<ServerBuildOptions>;
+	const cwd = process.cwd();
+	const outputDir = join(cwd, outDir);
+
+	if (!existsSync(outputDir)) {
+		await mkdir(outputDir, { recursive: true });
+	}
+
+	const source = _generatorServerEntryCode(port, options, config);
+
+	await _build(outputDir, source);
+
+	console.log(`\n[${name}]Builded a independently service in`, pc.green(outputDir), "\n");
+}
+
+async function _build(outputDir: string, source: string) {
+	const fileSrc = path.join(outputDir, "index-src.js");
+	const fileDest = path.join(outputDir, "index.js");
+	await writeFile(fileSrc, source, "utf-8");
+	const esBuildConfig = _createEsbuildConfig(fileSrc, fileDest);
+	await esbuild.build(esBuildConfig);
+	await rm(fileSrc);
+}
+
+function _createEsbuildConfig(fileSrc: string, fileDest: string): esbuild.BuildOptions {
+	return {
+		platform: "node",
+		format: "esm",
+		bundle: true,
+		external: ["fsevents"],
+		resolveExtensions: [".mjs", ".js", ".mts", ".ts", ".json"],
+		entryPoints: [fileSrc],
+		outfile: fileDest,
+		banner: { js: "const require = createRequire(import.meta.url);" },
+	};
+}
+
+function _generatorServerEntryCode(port: number, options: ResolvePluginOptionsType, config: ResolvedConfig) {
+	const mockFiles: string[] = [];
+	for (const pathSrc of options.include) {
+		mockFiles.push(`mockFiles.push(()=>import('../${pathSrc}'))`);
+	}
+
+	const options2 = { ...options, include: ["mock"] };
+	return `import connect from "connect";
+${options.cors ? 'import cors from "cors";' : ""}
+import { createFakeMiddleware, createLogger } from "${name}";
+
+const loggerOutput = createLogger(${JSON.stringify(config.logLevel)}, {
+	allowClearScreen: ${config.clearScreen},
+	// customLogger: ${config.customLogger},
+});
+
+const mockFiles=[];
+${mockFiles.join("\n")}
+
+async function main() {
+
+	const app = connect();
+	${options.cors ? "app.use(cors());" : ""}
+	const middleware = await createFakeMiddleware(
+		{
+			...${JSON.stringify(options2, null, 2)},
+			loggerOutput,
+			// config.root
+			root: process.cwd(),
+      mockFiles,
+		},
+		app
+	);
+	app.use(middleware);
+
+	app.listen(${port});
+	console.log("listen: http://localhost:${port}");
+}
+
+main();
+`;
+}
